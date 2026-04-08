4bc74e1623e1b5ce71439042fe9a109e72b0d5cf
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-04-08 16:19:52 +0200

    incr

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.VKVideo.Mixfile do
   use Mix.Project
 
-  @version "0.2.3"
+  @version "0.2.4"
   @github_url "https://github.com/membraneframework/membrane_vk_video_plugin"
 
   def project do
